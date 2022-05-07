82c297b49a12d00712061ea3bccbcc1a48e25b5b
Noriyuki <norifxsc@gmail.com>
2022-05-08 00:24:45 +0900

    add double_vibration

---
+++ src/double_vibration.rs
@@ -0,0 +1,58 @@
+use crate::traits::*;
+use ndarray::*;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Vibration1 {
+    pub k_1: f64,
+    pub m_1: f64,
+    pub k_2: f64,
+    pub m_2: f64,
+}
+
+impl Default for Vibration1 {
+    fn default() -> Self {
+        Vibration1 {
+            k_1: 1.0,
+            m_1: 2.0,
+            k_2: 2.0,
+            m_2: 4.0,
+        }
+    }
+}
+
+impl Vibration1 {
+    pub fn new(k_1: f64, m_1: f64, k_2: f64, m_2: f64) -> Self {
+        Vibration1 {
+            k_1: k_1,
+            m_1: m_1,
+            k_2: k_2,
+            m_2: m_2,
+        }
+    }
+}
+
+impl ModelSpec for Vibration1 {
+    type Scalar = f64;
+    type Dim = Ix1;
+
+    fn model_size(&self) -> usize {
+        4
+    }
+}
+
+impl Explicit for Vibration1 {
+    fn rhs<'a, S>(&mut self, a: &'a mut ArrayBase<S, Ix1>) -> &'a mut ArrayBase<S, Ix1>
+    where
+        S: DataMut<Elem = f64>,
+    {
+        let v_1 = a[0];
+        let u_1 = a[1];
+        let v_2 = a[2];
+        let u_2 = a[3];
+        a[0] = -2. * self.k_1 / self.m_1 * u_1 + self.k_2 / self.m_2 * u_2;
+        a[1] = v_1;
+        a[2] = self.k_1 / self.m_1 * u_1 - 2. * self.k_2 / self.m_2 * u_2;
+        a[3] = v_2;
+        a
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use eom::traits::*;
 use eom::*;
 use ndarray::arr1;
+mod double_vibration;
 mod single_vibration;
 
 fn main() {
     let dt = 0.01;
     //let eom = ode::Lorenz63::default();
-    let eom = single_vibration::Vibration1::default();
+    //let eom = single_vibration::Vibration1::default();
+    let eom = double_vibration::Vibration1::default();
     let mut teo = explicit::RK4::new(eom, dt);
-    let ts = adaptor::time_series(arr1(&[1.0, 1.0]), &mut teo);
+    let ts = adaptor::time_series(arr1(&[0., 0.2, 0., 0.4]), &mut teo);
     let end_time = 10000;
-    println!("time,u,v,");
+    println!("time,v_1,u_1,v_2,u_2");
     for (t, v) in ts.take(end_time).enumerate() {
-        println!("{},{},{}", t as f64 * dt, v[0], v[1]);
+        println!("{},{},{},{},{}", t as f64 * dt, v[0], v[1], v[2], v[3]);
     }
 }
