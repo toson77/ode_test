4c3db55e8cad3195477f77740a58e4eb66da845b
toson77 <t182324@cc.utsunomiya-u.ac.jp>
2022-05-19 19:07:10 +0900

    fix

--- src/double_vibration.rs
+++ src/double_vibration.rs
@@ -3,29 +3,26 @@ use ndarray::*;
 
 #[derive(Clone, Copy, Debug)]
 pub struct Vibration1 {
-    pub k_1: f64,
+    pub k: f64,
     pub m_1: f64,
-    pub k_2: f64,
     pub m_2: f64,
 }
 
 impl Default for Vibration1 {
     fn default() -> Self {
         Vibration1 {
-            k_1: 1.0,
+            k: 1.0,
             m_1: 2.0,
-            k_2: 2.0,
             m_2: 4.0,
         }
     }
 }
 
 impl Vibration1 {
-    pub fn new(k_1: f64, m_1: f64, k_2: f64, m_2: f64) -> Self {
+    pub fn new(k: f64, m_1: f64, m_2: f64) -> Self {
         Vibration1 {
-            k_1: k_1,
+            k: k,
             m_1: m_1,
-            k_2: k_2,
             m_2: m_2,
         }
     }
@@ -49,9 +46,9 @@ impl Explicit for Vibration1 {
         let u_1 = a[1];
         let v_2 = a[2];
         let u_2 = a[3];
-        a[0] = -2. * self.k_1 / self.m_1 * u_1 + self.k_2 / self.m_2 * u_2;
+        a[0] = -2. * self.k / self.m_1 * u_1 + self.k / self.m_2 * u_2;
         a[1] = v_1;
-        a[2] = self.k_1 / self.m_1 * u_1 - 2. * self.k_2 / self.m_2 * u_2;
+        a[2] = self.k / self.m_1 * u_1 - 2. * self.k / self.m_2 * u_2;
         a[3] = v_2;
         a
     }
